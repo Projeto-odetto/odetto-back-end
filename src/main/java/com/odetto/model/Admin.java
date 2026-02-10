ebc9936fd110c29f49ac85b18b711e6dd4dd94a2
carvalho-joao <joao.carvalho@germinare.org.br>
2026-02-10 11:23:44 -0300

    subindo mvc (toma marcos)

---
+++ src/main/java/com/odetto/model/Admin.java
@@ -0,0 +1,23 @@
+package com.odetto.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Admin {
+    @Id
+    @GeneratedValue(strategy = jakarta.persistence.GenerationType.IDENTITY)
+    private Long id;
+    private String email;
+    private String password;
+
+}
