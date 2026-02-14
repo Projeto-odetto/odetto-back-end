0d8f2f16b9927926edfbb787e92936c3ef83aaec
GustavoJovelliGerminare <157593078+GustavoJovelliGerminare@users.noreply.github.com>
2026-02-14 20:51:43 -0300

    Resolvendo conflitos em Admin

--- src/main/java/com/odetto/service/AdminService.java
+++ src/main/java/com/odetto/service/AdminService.java
@@ -1,19 +1,13 @@
 package com.odetto.service;
 
-<<<<<<< HEAD
-=======
 import com.odetto.dto.Admin.AdminResponseDTO;
->>>>>>> 814bcf09fc4a851dcf9f536425a72c894bbffe11
 import com.odetto.repository.AdminRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import tools.jackson.databind.ObjectMapper;
 
-<<<<<<< HEAD
-=======
 import java.util.List;
 
->>>>>>> 814bcf09fc4a851dcf9f536425a72c894bbffe11
 @Service
 public class AdminService {
     private final AdminRepository adminRepository;
@@ -25,15 +19,11 @@ public class AdminService {
         this.objectMapper = objectMapper;
     }
 
-<<<<<<< HEAD
-=======
     public List<AdminResponseDTO> listAdmin() {
         List<AdminResponseDTO> adminResponseDTOS = adminRepository.findAll().stream()
                 .map(admin -> objectMapper.convertValue(admin, AdminResponseDTO.class))
                 .toList();
         return adminResponseDTOS;
     }
-
->>>>>>> 814bcf09fc4a851dcf9f536425a72c894bbffe11
 
 }
