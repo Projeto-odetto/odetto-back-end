11693815184494658b1c2c07322e1d627c314d52
GustavoJovelliGerminare <157593078+GustavoJovelliGerminare@users.noreply.github.com>
2026-02-14 20:48:21 -0300

    Resolver Conflitos de Admin

--- src/main/java/com/odetto/controller/AdminController.java
+++ src/main/java/com/odetto/controller/AdminController.java
@@ -1,13 +1,5 @@
 package com.odetto.controller;
 
-
-<<<<<<< HEAD
-import com.odetto.service.AdminService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-=======
 import com.odetto.dto.Admin.AdminRequestDTO;
 import com.odetto.dto.Admin.AdminResponseDTO;
 import com.odetto.service.AdminService;
@@ -19,28 +11,19 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
->>>>>>> 814bcf09fc4a851dcf9f536425a72c894bbffe11
 @RestController
 @RequestMapping("/api/admin")
 public class AdminController {
     private final AdminService adminService;
 
-<<<<<<< HEAD
     @Autowired
-=======
->>>>>>> 814bcf09fc4a851dcf9f536425a72c894bbffe11
     public AdminController(AdminService adminService) {
         this.adminService = adminService;
     }
 
-<<<<<<< HEAD
-=======
+
     @GetMapping("/list-admins")
     public ResponseEntity<List<AdminResponseDTO>> listAdmins() {
         List<AdminResponseDTO> admins = adminService.listAdmin();
         return ResponseEntity.ok(admins);
     }
-
-
->>>>>>> 814bcf09fc4a851dcf9f536425a72c894bbffe11
-}
