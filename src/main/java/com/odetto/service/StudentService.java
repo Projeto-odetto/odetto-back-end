53b2f841f76c6524e11eab5acb0c2636d8cd7dd5
GustavoJovelliGerminare <157593078+GustavoJovelliGerminare@users.noreply.github.com>
2026-02-14 20:53:02 -0300

    Resolvendo conflitos em Student

--- src/main/java/com/odetto/service/StudentService.java
+++ src/main/java/com/odetto/service/StudentService.java
@@ -1,12 +1,8 @@
 package com.odetto.service;
 
-<<<<<<< HEAD
-import com.odetto.dto.Student.StudentResponseDTO;
-=======
 import com.odetto.dto.Student.StudentRequestDTO;
 import com.odetto.dto.Student.StudentResponseDTO;
 import com.odetto.model.Student;
->>>>>>> 814bcf09fc4a851dcf9f536425a72c894bbffe11
 import com.odetto.repository.StudentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -32,15 +28,9 @@ public class StudentService {
         return studentResponseDTOS;
     }
 
-<<<<<<< HEAD
-    public StudentResponseDTO insertStudent(StudentResponseDTO student) {
-        var studentEntity = objectMapper.convertValue(student, com.odetto.model.Student.class);
-        var savedStudent = studentRepository.save(studentEntity);
-=======
     public StudentResponseDTO insertStudent(StudentRequestDTO student) {
         Student studentEntity = objectMapper.convertValue(student, Student.class);
         Student savedStudent = studentRepository.save(studentEntity);
->>>>>>> 814bcf09fc4a851dcf9f536425a72c894bbffe11
         return objectMapper.convertValue(savedStudent, StudentResponseDTO.class);
     }
 
