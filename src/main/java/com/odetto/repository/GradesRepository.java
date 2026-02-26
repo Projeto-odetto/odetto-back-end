58f5efe0476b279bfbb3379359dce3174d2aede6
carvalho-joao <joao.carvalho@germinare.org.br>
2026-02-25 21:08:34 -0300

    subindo o endpoint da listagem de boletim

--- src/main/java/com/odetto/repository/GradesRepository.java
+++ src/main/java/com/odetto/repository/GradesRepository.java
@@ -2,19 +2,28 @@ package com.odetto.repository;
 
 import com.odetto.dto.Grades.StudentGradeResponseDTO;
 import com.odetto.model.Grades;
+import com.odetto.projection.StudentGradeProjection;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
 import java.util.List;
 
 
 public interface GradesRepository extends JpaRepository<Grades,Long> {
-    @Query(value = "select " +
-            " com.odetto.dto.Grades.StudentGradeResponseDTO(s2.name, t.name, g.grade,  ROUND((SELECT AVG(elem) FROM unnest(g.grade) AS elem)::numeric, 2) AS media_do_array) from grades g\n" +
-            "join report_card rc on g.report_card_id = rc.id \n" +
-            "join student s on rc.enrollment_student = s.enrollment \n" +
-            "join subjects s2 on s2.id = g.subject_id \n" +
-            "join subject_teacher st on st.id_subject = s2.id \n" +
-            "join teacher t on t.cpf = st.cpf_teacher \n" +
-            "where s.enrollment = :enrollmentStudent", nativeQuery = true)
-    List<StudentGradeResponseDTO> findGradesByEnrollmentStudent(Long enrollmentStudent);
+    @Query(value = """
+        select 
+            s2.name as subjectName,
+            t.name as teacherName,
+            g.grade as grades,
+            ROUND((SELECT AVG(elem) FROM unnest(g.grade) elem)::numeric, 2) as average
+        from grades g
+        join report_card rc on g.report_card_id = rc.id
+        join student s on rc.enrollment_student = s.enrollment
+        join subjects s2 on s2.id = g.subject_id
+        join subject_teacher st on st.id_subject = s2.id
+        join teacher t on t.cpf = st.cpf_teacher
+        where s.enrollment = :enrollmentStudent
+        """, nativeQuery = true)
+    List<StudentGradeProjection> findGradesByEnrollmentStudent(@Param("enrollmentStudent") Long enrollmentStudent);
 }
