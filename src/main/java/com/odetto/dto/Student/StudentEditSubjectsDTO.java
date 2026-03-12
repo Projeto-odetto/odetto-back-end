2d1d3acf52b9136c13d8ec2ed759a449bdf57c6d
Gustavo Munhoz Adario Jovelli <gustavo.jovelli@germinare.org.br>
2026-03-12 08:54:12 -0300

    Realizando Ajustes

--- src/main/java/com/odetto/dto/Student/StudentEditSubjectsDTO.java
+++ src/main/java/com/odetto/dto/Student/StudentEditSubjectsDTO.java
@@ -1,4 +1,12 @@
 package com.odetto.dto.Student;
 
+import lombok.Data;
+
+import java.util.List;
+
+@Data
 public class StudentEditSubjectsDTO {
+    private Long enrollment;
+    private List<String> addedSubjects;
+    private List<String> removedSubjects;
 }
